4435220ffec4b0eae69ebcf63cdad9b6602a1a0c
raaksss <rakshitamanocha@gmail.com>
2024-04-28 16:34:51 +0530

    Minor Changes

--- frontend/src/lib/DobInput.js
+++ frontend/src/lib/DobInput.js
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
 import { TextField } from "@material-ui/core";
 
-const DobInput = ({ label, value, onChange, error, helperText, className }) => {
-  const [dobError, setDobError] = useState(""); // State variable for date of birth error
-
+const DobInput = ({ label, value, onChange, helperText, className }) => {
+  const [dobError, setDobError] = useState(""); 
   const handleChange = (event) => {
     const dob = event.target.value;
     onChange(dob);
@@ -11,19 +10,19 @@ const DobInput = ({ label, value, onChange, error, helperText, className }) => {
 
   const handleBlur = () => {
     if (!value) {
-      onChange(""); // Update parent component with empty string if DOB is empty
+      onChange(""); 
       return;
     }
 
     const selectedDate = new Date(value);
     const today = new Date();
-    const maxDate = new Date(today.getFullYear() - 55, today.getMonth(), today.getDate()); // Max date for 55 years old
+    const maxDate = new Date(today.getFullYear() - 55, today.getMonth(), today.getDate()); 
 
     if (selectedDate <= maxDate) {
-      onChange(value); // Update parent component with valid DOB
-      setDobError(""); // Clear any previous error
+      onChange(value); 
+      setDobError(""); 
     } else {
-      setDobError("You must be at least 55 years old."); // Set error message
+      setDobError("You must be at least 55 years old."); 
     }
   };
 
@@ -36,8 +35,8 @@ const DobInput = ({ label, value, onChange, error, helperText, className }) => {
         value={value}
         onChange={handleChange}
         onBlur={handleBlur}
-        error={!!dobError} // Set error state based on the presence of error message
-        helperText={dobError || helperText} // Use error message if present, otherwise use helper text
+        error={!!dobError} 
+        helperText={dobError || helperText} 
         InputLabelProps={{
           shrink: true,
         }}

2c844db2cebfc12f912a6d17ad175058952ab2cb
raaksss <rakshitamanocha1@gmail.com>
2024-06-05 12:32:49 +0530

    uploadRoutes changed a bit to support ESM

--- backend/routes/uploadRoutes.js
+++ backend/routes/uploadRoutes.js
@@ -1,69 +1,76 @@
 const express = require("express");
-const multer = require("multer");
 const fs = require("fs");
 const { v4: uuidv4 } = require("uuid");
 const { promisify } = require("util");
 
-const pipeline = promisify(require("stream").pipeline);
-
 const router = express.Router();
 
-const upload = multer();
-
-router.post("/resume", upload.single("file"), (req, res) => {
-  const { file } = req;
-  if (file.detectedFileExtension != ".pdf") {
-    res.status(400).json({
-      message: "Invalid format",
-    });
-  } else {
+router.post("/resume", async (req, res) => {
+  try {
+    const multer = (await import("multer")).default;
+    const pipeline = promisify(require("stream").pipeline);
+    
+    const upload = multer();
+    const { file } = req;
+    
+    if (file.detectedFileExtension !== ".pdf") {
+      return res.status(400).json({
+        message: "Invalid format",
+      });
+    }
+    
     const filename = `${uuidv4()}${file.detectedFileExtension}`;
 
-    pipeline(
+    await pipeline(
       file.stream,
       fs.createWriteStream(`${__dirname}/../public/resume/${filename}`)
-    )
-      .then(() => {
-        res.send({
-          message: "File uploaded successfully",
-          url: `/host/resume/${filename}`,
-        });
-      })
-      .catch((err) => {
-        res.status(400).json({
-          message: "Error while uploading",
-        });
-      });
+    );
+
+    res.send({
+      message: "File uploaded successfully",
+      url: `/host/resume/${filename}`,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({
+      message: "Error while uploading",
+    });
   }
 });
 
-router.post("/profile", upload.single("file"), (req, res) => {
-  const { file } = req;
-  if (
-    file.detectedFileExtension != ".jpg" &&
-    file.detectedFileExtension != ".png"
-  ) {
-    res.status(400).json({
-      message: "Invalid format",
-    });
-  } else {
+router.post("/profile", async (req, res) => {
+  try {
+    const multer = (await import("multer")).default;
+    const pipeline = promisify(require("stream").pipeline);
+    
+    const upload = multer();
+    const { file } = req;
+
+    if (
+      file.detectedFileExtension !== ".jpg" &&
+      file.detectedFileExtension !== ".png"
+    ) {
+      return res.status(400).json({
+        message: "Invalid format",
+      });
+    }
+
     const filename = `${uuidv4()}${file.detectedFileExtension}`;
 
-    pipeline(
+    await pipeline(
       file.stream,
       fs.createWriteStream(`${__dirname}/../public/profile/${filename}`)
-    )
-      .then(() => {
-        res.send({
-          message: "Profile image uploaded successfully",
-          url: `/host/profile/${filename}`,
-        });
-      })
-      .catch((err) => {
-        res.status(400).json({
-          message: "Error while uploading",
-        });
-      });
+    );
+
+    res.send({
+      message: "Profile image uploaded successfully",
+      url: `/host/profile/${filename}`,
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(400).json({
+      message: "Error while uploading",
+    });
   }
 });
 
